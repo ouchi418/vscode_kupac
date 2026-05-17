dfccb00eb70f1cedc5f7d02ab147a3a53efe6c21
uchi <yutauchi222@gmail.comgit config --global user.email yutauchi222@gmail.com>
2026-05-17 16:13:38 +0900

    hattten

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,22 +1,22 @@
-def fizz_buzz(n):
-    if n % 3 == 0 and n % 5 == 0:
-        return 'FizzBuzz'
-    elif n % 3 == 0:
-        return 'Fizz'
-    elif n % 5 == 0:
-        return 'Buzz'
-    else:
-        return str(n)
-    
-print(fizz_buzz(int(input("入力してください"))))
-
-#def fizz_buzz(n, x, y):
-#    if n % x == 0 and n % y == 0:
+#def fizz_buzz(n):
+#    if n % 3 == 0 and n % 5 == 0:
 #        return 'FizzBuzz'
-#    elif n % x == 0:
+#    elif n % 3 == 0:
 #        return 'Fizz'
-#    elif n % y == 0:
+#    elif n % 5 == 0:
 #        return 'Buzz'
 #    else:
 #        return str(n)
-    
+#    
+#print(fizz_buzz(int(input("入力してください"))))
+
+def fizz_buzz(n, x, y):
+    if n % x == 0 and n % y == 0:
+        return 'FizzBuzz'
+    elif n % x == 0:
+        return 'Fizz'
+    elif n % y == 0:
+        return 'Buzz'
+    else:
+        return str(n)
+   
